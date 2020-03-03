27ec3c13480afe94d13220aab286ca372a67081a
easec <mats.johannesson@easec.se>
2020-03-03 19:51:10 +0100

    Create installation.sh

---
+++ installation.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo -n "Hämtar signeringsnyckel"
+
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key ad$
+
+echo -n "Lägger till referenser"
+sudo apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main"
+
+echo -n "Uppdaterar referenser"
+sudo apt-get update
+
+echo -n "Installerar kubeadmin, kubelet och kubectl"
+
+sudo apt-get install -y kubeadm kubelet kubectl
+
+
+echo -n "Markerar att kubeadmin, kubelet och kubectl inte uppdateras"
+
+sudo apt-mark hold kubeadm kubelet kubectl
